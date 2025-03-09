a69065cb6833a9144560fb84be34f3c3077de7d1
TaranshChellani <taransh@suretynow.com>
2025-03-09 23:28:17 +0530

    Added Hero Section

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="relative w-full h-[120vh] sm:h-[100vh]">
+      {/* overlay  */}
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
+      {/* <img
+        src="/images/hero12.jpg"
+        alt="hero"
+        className="w-full h-full object-cover"
+      /> */}
+      <video
+        src="/images/hero1.mp4"
+        autoPlay
+        muted
+        loop
+        preload="metadata"
+        className="w-full h-full object-cover"
+      />
+      {/* text  */}
+      <div className="absolute top-[50%] left-[50%] z-[100] w-full h-full translate-x-[-50%] translate-y-[-50%]">
+        <div className="flex items-center justify-center flex-col w-full h-full">
+          <div>
+            <h1 className="text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] text-white font-bold tracking-[0.7rem] uppercase">
+              Lets explore the temples
+            </h1>
+            <p className="text-white text-center text-lg font-normal [word-spacing:5px] md:text-base">
+              Get best prices and deals on tours
+            </p>
+            <button
+              onClick={() =>
+                window.open("https://wa.me/+919118859567", "_blank")
+              }
+              className="bg-yellow-300 text-black px-4 py-2 rounded-md mt-4 mx-auto block hover:bg-yellow-500 transition-colors"
+            >
+              Book Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,33 +1,60 @@
-import { navLinks } from '@/constant/constant'
-import Link from 'next/link'
-import React from 'react'
-import { HiBars3BottomRight } from 'react-icons/hi2'
-import { TbAirBalloon } from 'react-icons/tb'
+"use client";
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { TbAirBalloon } from "react-icons/tb";
 
-const Nav = () => {
+type props = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: props) => {
+  const [navBg, setNavBg] = useState(false);
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  });
   return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full'>
-        <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
-            {/* logo */}
-            <div className="flex items-center space-x-2">
-                <div className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col">
-                    <TbAirBalloon className='w-6 h-6 text-white'/>
-                </div>
-                <h1 className='text-xl md:text-2xl  text-white uppercase font-bold'>Teerthayatrik</h1>
-            </div>
-            {/* navlinks */}
-            <div className='hidden lg:flex items-center space-x-10'>
-                {navLinks.map((link)=>{
-                    return <Link href={link.url} key={link.id}>
-                        <p className='relative text-white text-base font-medium w-fit block after:block after:content-[""] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition duration-300 after:origin-right'>{link.label}</p>
-                        </Link>
-                })}
-            </div>
-            {/* burger-menu  */}
-            <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
+    <div
+      className={`transition-all duration-200 h-[12vh] z-[1000] fixed w-full ${
+        navBg ? "shadow-md bg-blue-950" : "fixed"
+      }`}
+    >
+      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        {/* logo */}
+        <div className="flex items-center space-x-2">
+          <div className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col">
+            <TbAirBalloon className="w-6 h-6 text-white" />
+          </div>
+          <h1 className="text-xl md:text-2xl  text-white uppercase font-bold">
+            Teerthayatrik
+          </h1>
         </div>
+        {/* navlinks */}
+        <div className="hidden lg:flex items-center space-x-10">
+          {navLinks.map((link) => {
+            return (
+              <Link href={link.url} key={link.id}>
+                <p className='relative text-white text-base font-medium w-fit block after:block after:content-[""] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition duration-300 after:origin-right'>
+                  {link.label}
+                </p>
+              </Link>
+            );
+          })}
+        </div>
+        {/* burger-menu  */}
+        <HiBars3BottomRight
+          onClick={openNav}
+          className="w-8 h-8 cursor-pointer text-white lg:hidden"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
