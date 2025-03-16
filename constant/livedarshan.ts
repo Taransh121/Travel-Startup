b6207e15c8d10aa20f0fe790b6d211700ced78d6
AdityaB786 <126441346+AdityaB786@users.noreply.github.com>
2025-03-16 17:38:16 +0530

    livedarshan

---
+++ constant/livedarshan.ts
@@ -0,0 +1,51 @@
+export const livedarshan = [
+    {
+      title: "Vindhyachal Temple",
+      slug: "vindhyachal-temple",
+      imageUrl: "/images/livedarshan/Maa vindhyawasini Vindhyanchal_.jpg",
+      description: "A sacred Shaktipeeth of Maa Vindhyavasini, known for wish fulfillment and divine blessings.",
+    },
+    {
+      title: "Kali Kho Temple",
+      slug: "kali-kho-temple",
+      imageUrl: "/images/livedarshan/Kali kho_.jpg",
+      description: "A powerful Siddhpeeth of Maa Kali, offering protection and strength.",
+    },
+    {
+      title: "Bade Hanuman Ji (Prayagraj)",
+      slug: "bade-hanuman-prayagraj",
+      imageUrl: "/images/livedarshan/Bade Hanuman_.jpg",
+      description: "Unique reclining Hanuman idol, bringing courage and divine protection.",
+    },
+    {
+      title: "Ganga Aarti Prayagraj",
+      slug: "ganga-aarti-prayagraj",
+      imageUrl: "/images/livedarshan/Prayagraj Ganga arti.jpg",
+      description: "A soul-stirring ritual on the Ganges, believed to cleanse sins and uplift the spirit.",
+    },
+    {
+      title: "Someshwar Mahadev (Prayagraj)",
+      slug: "someshwar-mahadev-prayagraj",
+      imageUrl: "/images/livedarshan/Someshwar mahadev.jpg",
+      description: "Ancient Shiva temple, blessing devotees with peace and prosperity.",
+    },
+    {
+      title: "Sooltankeshwar Mahadev (Prayagraj)",
+      slug: "sooltankeshwar-mahadev-prayagraj",
+      imageUrl: "/images/livedarshan/Shooltankeshwar mahadev_.jpg",
+      description: "A revered temple granting wishes and spiritual power.",
+    },
+    {
+      title: "Ganga Aarti Varanasi",
+      slug: "ganga-aarti-varanasi",
+      imageUrl: "/images/livedarshan/Varanasi ganga arti.jpg",
+      description: "A soul-stirring ritual on the Ganges, believed to cleanse sins and uplift the spirit.",
+    },
+    {
+      title: "Sarva Darshan",
+      slug: "sarva-darshan",
+      imageUrl: "/images/livedarshan/Kali kho_.jpg",
+      description: "Daily live darshans from all temples—your divine connection, anytime, anywhere.",
+    },
+  ];
+  
