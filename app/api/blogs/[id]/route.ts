4942992dd00453173a14b4aa1303fe2c21444b71
TARANSH CHELLANI <taranshchellani121@gmail.com>
2025-03-20 15:40:28 +0530

    minor fixes

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -31,8 +31,9 @@
 //   }
 // }
 
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   return NextResponse.json({ message: "Hello, World!" }, { status: 200 });
 }
+
